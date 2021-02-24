dd9ec178226619bcad08e3e4d3a1d79252d73b44
Enoch2k2 <enoch2k2@hotmail.com>
2021-02-24 11:15:36 -0600

    added blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Blog.create([
+  { title: "Blog Title", content: "This is a blog" },
+  { title: "Blog Title 2", content: "This is a blog 2" },
+  { title: "Blog Title 3", content: "This is a blog 3" }
+])
